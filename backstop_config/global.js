e75a88919ef84ff7b1b2ab76c5d7435689533bbe
Leslie Doherty <ldoherty@tableau.com>
2020-08-21 17:28:17 -0700

    Fix bug when passing argument for reference environment

--- backstop_config/global.js
+++ backstop_config/global.js
@@ -18,8 +18,8 @@ const STAGING = {
   siteUrl: process.env.VR_STAGING
 }
 
-const TEST = args.testEnv ? { name: 'override test', siteUrl: args.testEnv } : LOCAL
-const REFERENCE = args.referenceEnv ? { name: 'override reference', siteUrl: args.reference } : STAGING
+const TEST = args.testEnv ? { name: 'override-test', siteUrl: args.testEnv } : LOCAL
+const REFERENCE = args.referenceEnv ? { name: 'override-reference', siteUrl: args.referenceEnv } : STAGING
 const PROJECT_ID = args.projectID ? args.projectID + '_regression' : TEST.name + '_regression'
 
 module.exports = exports = { PRODUCTION, STAGING, LOCAL, TEST, REFERENCE, PROJECT_ID }
