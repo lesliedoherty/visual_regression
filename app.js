e86e63ea619dcbbd0ba4b545588401af5ca27531
Leslie Doherty <ldoherty@tableau.com>
2020-08-21 19:19:56 -0700

    [WIP] Committing in the event my machine or I blow up before I can get back to this. It's broken but in progress.

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 #! /usr/bin/env node --no-warnings
 const args = require('yargs').argv
+const backstop = require('backstopjs')
 const scenario = require('./backstop_config/scenarios/demo')
 const { TEST, REFERENCE, PROJECT_ID } = require('./backstop_config/global')
+console.log('⭐️️  Running test for ' + PROJECT_ID)
+console.log('⭐️  Testing environment ' + TEST.siteUrl)
+console.log('⭐️  Reference environment ' + REFERENCE.siteUrl)
 
 const projectConfig = require('./backstop_config/backstop.config.js')({
   project: PROJECT_ID,
@@ -26,15 +30,32 @@ if (args.approve) {
   commandToRun = 'approve'
 }
 
+if (commandToRun !== '') {
+  backstop(commandToRun, { config: projectConfig })
+}
+
 function getScenariosForProject ({ siteUrl }) {
-  const scenarios = scenario.map(({ label, url }) => {
+  const scenarios = scenario.map(({ label, url, extend }) => {
+    let options = []
+    if (extend) {
+      for (const [key, value] of Object.entries(extend)) {
+        options = `${key}: ${value}`
+        console.log('options here are ' + options)
+      }
+    }
     return {
       label: label,
       url: `${siteUrl}/${url}`,
-      referenceUrl: `${REFERENCE.siteUrl}/${url}`
+      referenceUrl: `${REFERENCE.siteUrl}/${url}`,
+      options
     }
   })
+  for (let k = 0; k < scenarios.length; k++) {
+    console.log('🔗  Reference url ' + scenarios[k].referenceUrl)
+    console.log('⚖️  Testing url ' + scenarios[k].url)
+    console.log('⚖️  Testing options ' + scenarios[k].options)
+  }
   return scenarios
 }
 
-module.exports = exports = { commandToRun, getScenariosForProject, projectConfig }
+module.exports = exports = { commandToRun, projectConfig }
